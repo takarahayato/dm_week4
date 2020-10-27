7c0f1937fa5056ed9db20160ea39faf6de6225fb
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2020-10-27 17:36:34 +0900

    regression.fit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import datasets
 import regression
+import importlib
+importlib.reload(regression)
 X,Y= datasets.load_linear_example1()
-print(X)
-print(X[0])
-print(Y)
+# print(X)
+# print(X[0])
+# print(Y)
 
 model = regression.LinearRegression()
-
+model.fit(X,Y)
+model.theta
+print(model.theta)
